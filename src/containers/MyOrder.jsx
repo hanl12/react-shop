65fd644b7e241f0519e9e4c9702e35e9e946a9f4
HenryN <hyrre9056@gmail.com>
2021-10-15 15:15:18 -0500

    Adding total sum for the cart

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -8,6 +8,13 @@ const MyOrder = () => {
   const {
     state: { cart },
   } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+	  const sum = cart.reduce(reducer, 0);
+	  return sum;
+  };
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -15,13 +22,15 @@ const MyOrder = () => {
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
-        {cart.map((product) => <OrderItem product={product} key={`orderItem-${product.id}`} />)}
-        
+        {cart.map((product) => (
+          <OrderItem product={product} key={`orderItem-${product.id}`} />
+        ))}
+
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>$ {sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
